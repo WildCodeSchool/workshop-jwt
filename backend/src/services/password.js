45e2c8f75c19ae52bd464eeefe51d6e794b7d087
bastienwcs <bastien.krettly@wildcodeschool.com>
2022-06-09 10:09:28 +0200

    add argon2 config

--- backend/src/services/password.js
+++ backend/src/services/password.js
@@ -1,11 +1,18 @@
 const argon2 = require("argon2");
 
+const hashingOptions = {
+  type: argon2.argon2id,
+  memoryCost: 2 ** 16,
+  timeCost: 5,
+  parallelism: 1,
+};
+
 async function passwordHash(password) {
-  return argon2.hash(password);
+  return argon2.hash(password, hashingOptions);
 }
 
 async function passwordVerify(hash, password) {
-  return argon2.verify(hash, password);
+  return argon2.verify(hash, password, hashingOptions);
 }
 
 module.exports = { passwordHash, passwordVerify };
